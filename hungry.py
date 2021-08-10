6e3a665a2c873fa168c9b920162e07d63d981ac0
Pratima Maiti <pratima.maiti@nagarro.com>
2021-08-10 19:40:13 +0530

    Pizza

--- hungry.py
+++ hungry.py
@@ -1,5 +1,6 @@
-hungry=input("are your hungry?")
-if hungry=="yes":
+hungry = input("are your hungry?")
+if hungry == "yes":
     print("Eat momo")
+    print("Eat Pizza")
 else:
     print("Do your work")
